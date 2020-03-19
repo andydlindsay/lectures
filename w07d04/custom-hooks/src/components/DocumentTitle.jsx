464f8b90e92f4bd4f3f003a7063b7356c4362244
andydlindsay@gmail.com <andydlindsay@gmail.com>
2020-03-18 23:54:08 -0700

    refactor DocumentTitle component

--- w07d04/custom-hooks/src/components/DocumentTitle.jsx
+++ w07d04/custom-hooks/src/components/DocumentTitle.jsx
@@ -6,8 +6,12 @@ const DocumentTitle = () => {
   const { x, y } = useMousePosition();
   const title = `Mouse is at ${x}, ${y}`;
   useDocumentTitle(title);
+  
+  const style = {
+    fontSize: `${y / 5}px`
+  };
 
-  return (<h1>Document Title</h1>);
+  return (<h1 style={ style }>Document Title</h1>);
 };
 
 export default DocumentTitle;
