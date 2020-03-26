698363b0c67dc748606eb2515ef25f123d88deb7
andydlindsay@gmail.com <andydlindsay@gmail.com>
2020-03-25 17:58:20 -0700

    add api server to w4d3

--- w04d03/server.js
+++ w04d03/server.js
@@ -1,9 +1,32 @@
 const express = require('express');
+const bodyParser = require('body-parser');
 
 const app = express();
+const port = process.env.PORT || 8765;
+const users = require('./data/users.json');
+const posts = require('./data/posts.json');
 
+app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static('public'));
 
-app.listen(3000, () => {
-  console.log('app is listening on port 3000');
+app.get('/api/users', (req, res) => {
+  res.json(users);
+});
+
+app.get('/api/users/:id', (req, res) => {
+  res.json(users[req.params.id]);
+});
+
+app.get('/api/posts', (req, res) => {
+  res.json(posts);
+});
+
+app.post('/api/posts', (req, res) => {
+  const post = req.body;
+  posts.push(post);
+  res.status(201).json(post);
+});
+
+app.listen(port, () => {
+  console.log(`app is listening on port ${port}`);
 });
