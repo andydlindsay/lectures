de0781c20871dc19583c24dbbb29cc6d78c09728
andydlindsay@gmail.com <andydlindsay@gmail.com>
2020-03-27 11:54:31 -0700

    add q1 tests and solution

---
+++ breakout-mock-exam-review/q1.js
@@ -0,0 +1,67 @@
+/* Question 1
+ *
+ *  Implement the functions defined below
+ *
+ */
+
+/* ===========================================================================
+ * MIN - the lowest value in a list
+ *
+ * For example:
+ *
+ *    min([6,2,3,4,9,6,1,0,5])
+ *
+ * Returns:
+ *
+ *    0
+ */
+const min = function(arr) {
+  let smallest = Infinity;
+  for (const el of arr) {
+    if (el < smallest) {
+      smallest = el;
+    }
+  }
+  return smallest;
+};
+
+
+/* ===========================================================================
+ * MAX - the highest value in a list
+ *
+ * For example:
+ *
+ *    max([6,2,3,4,9,6,1,0,5])
+ *
+ * Returns:
+ *
+ *    9
+ */
+const max = function(arr) {
+  let largest = -Infinity;
+  for (const el of arr) {
+    if (el > largest) {
+      largest = el;
+    }
+  }
+  return largest;
+};
+
+/* ===========================================================================
+ * RANGE - the difference between the highest and lowest values in a list
+ *
+ * For example:
+ *
+ *    range([6,2,3,4,9,6,1,0,5])
+ *
+ * Returns:
+ *
+ *    9
+ */
+const range = function(arr) {
+  return max(arr) - min(arr);
+};
+
+// Don't change below:
+
+module.exports = { min, max, range };
