5ed58b7383122d4355f80e3ab36dc19dd6cf9d47
Andy Lindsay <andydlindsay@gmail.com>
2020-05-13 11:41:27 -0700

    update w7d2 notes

--- w07d02/immutable/src/App.js
+++ w07d02/immutable/src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
-import Pizza from './components/Pizza';
+// import Pizza from './components/Pizza';
+import PizzaTwo from './components/PizzaTwo';
 import './App.css';
 
 const App = () => {
   return (
     <div className="App">
-      <Pizza />
+      {/* <Pizza /> */}
+      <PizzaTwo />
     </div>
   );
 };
---
+++ w07d02/immutable/src/components/PizzaTwo.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const PizzaTwo = () => {
+  return (
+    <div className="pizza-two">
+      <h2>Pizza Round Two</h2>
+      
+    </div>
+  );
+};
+
+export default PizzaTwo;
