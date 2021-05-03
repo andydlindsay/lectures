3366f910b0cf696dbbbfbb4ff2def2cabee3f800
Andy Lindsay <andydlindsay@gmail.com>
2021-05-02 18:22:40 -0700

    done I think

--- w10d02/car-shop/app/controllers/cars_controller.rb
+++ w10d02/car-shop/app/controllers/cars_controller.rb
@@ -1,7 +1,7 @@
 class CarsController < ApplicationController
   def index
     if params[:search_term].length > 0
-      @cars = Car.joins(:model).where(model: { model: params[:search_term]})
+      @cars = Car.joins(:model).where("model LIKE ?", "%#{params[:search_term]}%")
     else
       @cars = Car.all
     end
--- w10d02/outline.md
+++ w10d02/outline.md
@@ -515,3 +515,15 @@ def show
   @car = Car.find(params[:id])
 end
 ```
+
+### Simple Search Functionality
+
+```rb
+def index
+  if params[:search_term].length > 0
+    @cars = Car.joins(:model).where("model LIKE ?", "%#{params[:search_term]}%")
+  else
+    @cars = Car.all
+  end
+end
+```
