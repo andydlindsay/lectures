6854dfcd7b6de1b8f8e9669a2b0fec85cda43593
Andy Lindsay <andydlindsay@gmail.com>
2021-06-27 19:31:16 -0700

    more updates

--- breakout-crud-with-ajax/server.js
+++ breakout-crud-with-ajax/server.js
@@ -10,9 +10,14 @@ const port = process.env.PORT || 3000;
 app.set('view engine', 'ejs');
 
 app.use(morgan('dev'));
+app.use(express.static("public"));
 app.use(express.urlencoded({ extended: false }));
 
 app.use('/products', productRouter);
+
+app.get('/', (req, res) => {
+  res.render('index');
+});
 
 app.listen(port, () => {
   console.log(`app is listening on port ${port}`);
