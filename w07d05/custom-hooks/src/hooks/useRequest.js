6f331309e42511cf86c6942f62896167e42baf84
Andy Lindsay <andydlindsay@gmail.com>
2021-12-14 18:08:51 -0800

    update useRequest hook definition

--- w07d05/custom-hooks/src/hooks/useRequest.js
+++ w07d05/custom-hooks/src/hooks/useRequest.js
@@ -2,25 +2,33 @@ import { useState, useEffect } from 'react';
 import axios from 'axios';
 
 const useRequest = (url) => {
-  const [data, setData] = useState({});
-  const [loading, setLoading] = useState(true);
-  const [errorMsg, setErrorMsg] = useState('');
+  const [state, setState] = useState({
+    data: null,
+    loading: true,
+    errorMsg: ''
+  });
 
   useEffect(() => {
     axios.get(url)
       .then(result => {
-        setLoading(false);
-        setData(result.data);
-        setErrorMsg('');
+        setState({
+          data: result.data,
+          loading: false,
+          errorMsg: ''
+        });
       })
       .catch(err => {
         console.log(err);
-        setErrorMsg('Error loading data');
-        setLoading(false);
-      });
-  }, []);
 
-  return { data, loading, errorMsg };
+        setState((prev) => ({
+          ...prev,
+          errorMsg: 'Error loading data',
+          loading: false
+        }));
+      });
+  }, [url]);
+
+  return state;
 };
 
 export default useRequest;
