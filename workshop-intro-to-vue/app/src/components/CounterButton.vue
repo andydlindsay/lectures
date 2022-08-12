f2527240230d6cff22badff741e93300cf586e3d
Andy Lindsay <andydlindsay@gmail.com>
2022-08-12 14:31:52 -0700

    workshop end

--- workshop-intro-to-vue/app/src/components/CounterButton.vue
+++ workshop-intro-to-vue/app/src/components/CounterButton.vue
@@ -10,7 +10,7 @@ export default {
   emits: ['increment'],
   methods: {
     handleClick() {
-      this.$emit('increment');
+      this.$emit('increment', 42);
     }
   }
 }
--- workshop-intro-to-vue/initial-browser/app.js
+++ workshop-intro-to-vue/initial-browser/app.js
@@ -1,8 +1,28 @@
 const app = Vue.createApp({
   data() {
     return {
-      message: 'Hello Vue!'
+      message: 'Hello Vue!',
+      visible: false,
+      sports: [
+        'football',
+        'volleyball',
+        'tiddlywinks',
+        'baseball',
+        'connect 4',
+        'kendo',
+        'beer pong'
+      ],
+      counter: 0,
+      newSport: ''
     }
+  },
+  methods: {
+    increment() {
+      this.counter += 1;
+    }
+  },
+  created() {
+    console.log('app has been created');
   }
 });
 
