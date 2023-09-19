5134bc9b8f0b859636095db68c48bcceaff496b9
Andy Lindsay <andydlindsay@gmail.com>
2023-09-19 14:16:08 -0700

    removes demo app from rails review lecture

--- w10d01/chatty/app/controllers/books_controller.rb
+++
@@ -1,6 +0,0 @@
-class BooksController < ApplicationController
-  def index
-    @author = Author.find(params[:author_id])
-    @books = @author.books
-  end
-end
--- w10d01/chatty/db/migrate/20200309070903_create_authors.rb
+++
@@ -1,9 +0,0 @@
-class CreateAuthors < ActiveRecord::Migration[6.0]
-  def change
-    create_table :authors do |t|
-      t.string :first_name
-      t.string :last_name
-      t.timestamps
-    end
-  end
-end
