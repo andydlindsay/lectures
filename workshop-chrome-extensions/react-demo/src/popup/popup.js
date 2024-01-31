c0d40f51d5fb3e22e15c82b8e94f05eefaf06b4d
Andy Lindsay <andydlindsay@gmail.com>
2024-01-31 15:03:52 -0800

    adds react-dom; no jsx compiler yet

--- workshop-chrome-extensions/react-demo/src/popup/popup.js
+++ workshop-chrome-extensions/react-demo/src/popup/popup.js
@@ -1,1 +1,11 @@
 import React from 'react';
+import ReactDOM from 'react-dom';
+
+const App = () => {
+  return (
+    <h2>Hello World</h2>
+  );
+};
+
+const root = ReactDOM.createRoot(document.getElementsByTagName('body'));
+root.render(<App />);
